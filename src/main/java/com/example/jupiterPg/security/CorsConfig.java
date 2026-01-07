3dfb8f0da35e55f3a2edb0d2f160bc504ce83305
rushikesh <rushikeshkoli87400@gmail.com>
2026-01-07 22:16:13 +0530

    new awscors added

--- src/main/java/com/example/jupiterPg/security/CorsConfig.java
+++ src/main/java/com/example/jupiterPg/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of( "http://13.60.229.108","http://localhost:3000"));
+        config.setAllowedOrigins(List.of( "http://13.60.173.109/","http://localhost:3000"));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
